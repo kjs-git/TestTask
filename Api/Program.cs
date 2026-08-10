afa03b709af5e6878e15240ebc54d3fb8a4f230c
Suhorukov <konstantinsuhorukov.main@gmail.com>
2026-08-11 06:16:10 +0700

    Add Db context

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,13 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ValueRecord> Values { get; set; }
+        public DbSet<FileResult> Results { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Настройка таблицы Values
+            modelBuilder.Entity<ValueRecord>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FileName).IsRequired().HasMaxLength(255);
+                entity.Property(e => e.Date).IsRequired();
+                entity.HasIndex(e => new { e.FileName, e.Date });
+            });
+
+            // Настройка таблицы Results
+            modelBuilder.Entity<FileResult>(entity =>
+            {
+                entity.HasKey(e => e.FileName);
+                entity.Property(e => e.FileName).HasMaxLength(255);
+
+                entity.HasIndex(e => e.MinDate);
+                entity.HasIndex(e => e.AverageValue);
+                entity.HasIndex(e => e.AverageExecutionTime);
+            });
+        }
+    }
+}
