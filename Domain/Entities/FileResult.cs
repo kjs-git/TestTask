4308a33dc0692eade723fdc05c2bad5c0c259abe
Suhorukov <konstantinsuhorukov.main@gmail.com>
2026-08-11 05:30:56 +0700

    Add domain models

---
+++ Domain/Entities/FileResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class FileResult
+    {
+        public string FileName { get; set; } = string.Empty;
+
+        public double DeltaTime { get; set; } // Дельта времени Date в секундах
+        public DateTime MinDate { get; set; } // Минимальное дата и время
+        public double AverageExecutionTime { get; set; } // Среднее время выполнения
+        public double AverageValue { get; set; } // Среднее значение по показателям
+        public double MedianValue { get; set; } // Медиана по показателям
+        public double MaxValue { get; set; } // Максимальное значение показателя
+        public double MinValue { get; set; } // Минимальное значение показателя
+    }
+}
---
+++ Domain/Entities/ValueRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class ValueRecord
+    {
+        public long Id { get; set; }
+        public string FileName { get; set; } = string.Empty; // Для привязки записи к конкретному файлу
+        public DateTime Date { get; set; } // Время начала операции
+        public int ExecutionTime { get; set; } // Время выполнения в секундах
+        public double Value { get; set; } // Показатель
+    }
+}
